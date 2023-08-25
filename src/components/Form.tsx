bbb06cfb6ee97558fa4b17b32fe2a3fc2c2f9b73
unknown <momlin069@gmail.com>
2023-08-25 11:34:16 +0900

    :zap: Imrove form handling

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,65 @@
+import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+
+interface FormValue {
+  username: string;
+  email: string;
+  channel: string;
+}
+
 export default function Form() {
+  const form = useForm<FormValue>();
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+
+  const onSubmit = (data: FormValue) => {
+    console.log(data);
+  };
+
   return (
     <div>
-      <form>
+      <h1>Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
-
+        <input
+          type="text"
+          id="username"
+          {...register("username", {
+            required: {
+              value: true,
+              message: "Username is Required",
+            },
+          })}
+        />
+        <p>{errors.username?.message}</p>
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
-
+        <input
+          type="email"
+          id="email"
+          {...register("email", {
+            pattern: {
+              value: /^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/,
+              message: "Invalid email format",
+            },
+          })}
+        />
+        <p>{errors.email?.message}</p>
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input
+          type="text"
+          id="channel"
+          {...register("channel", {
+            required: {
+              value: true,
+              message: "Channel is Required",
+            },
+          })}
+        />
+        <p>{errors.channel?.message}</p>
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />
     </div>
   );
 }
